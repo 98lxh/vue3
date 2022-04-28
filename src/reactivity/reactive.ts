c7aada7c1accfc3ac1f00d0be06c290c0288dca3
98lxh <75563939+98lxh@users.noreply.github.com>
2022-04-28 18:29:25 +0800

    feat(reactive):readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,16 +1,13 @@
-import { track, trigger } from "./effect"
+import { mutableHandlers, readonlyHandlers } from "./baseHandler"
 
 export function reactive(raw) {
-  return new Proxy(raw, {
-    get(target, key) {
-      const res = Reflect.get(target, key)
-      track(target, key)
-      return res
-    },
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value)
-      trigger(target, key)
-      return res
-    }
-  })
+  return createActiveObject(raw, mutableHandlers)
+}
+
+export function readonly(raw) {
+  return createActiveObject(raw, readonlyHandlers)
+}
+
+function createActiveObject(raw: any, baseHnadlers) {
+  return new Proxy(raw, baseHnadlers)
 }
