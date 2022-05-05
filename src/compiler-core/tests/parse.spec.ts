808fb31dff06f9c63df8d9d5b1604d92f34fb920
98lxh <75563939+98lxh@users.noreply.github.com>
2022-05-05 22:22:32 +0800

    feat(compiler-core):parse nested element

--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -20,11 +20,11 @@ describe('Parse', () => {
   describe('element', () => {
     it('simple element div', () => {
       const ast = baseParse('<div></div>')
-
       //root
       expect(ast.children[0]).toStrictEqual({
         type: NodeTypes.ElEMENT,
-        tag: 'div'
+        tag: 'div',
+        children: []
       })
     })
   })
@@ -39,5 +39,61 @@ describe('Parse', () => {
         content: 'some text'
       })
     })
+  })
+
+  test('hello world', () => {
+    const ast = baseParse('<p>hi,{{message}}</p>')
+    //root
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ElEMENT,
+      tag: 'p',
+      children: [
+        {
+          type: NodeTypes.TEXT,
+          content: 'hi,'
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message'
+          }
+        }
+      ]
+    })
+  })
+
+  test('Nested element', () => {
+    const ast = baseParse('<div><p>hi,</p>{{message}}</div>')
+    //root
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ElEMENT,
+      tag: 'div',
+      children: [
+        {
+          type: NodeTypes.ElEMENT,
+          tag: 'p',
+          children: [
+            {
+              type: NodeTypes.TEXT,
+              content: 'hi,'
+            }
+          ]
+        },
+        {
+          type: NodeTypes.INTERPOLATION,
+          content: {
+            type: NodeTypes.SIMPLE_EXPRESSION,
+            content: 'message'
+          }
+        }
+      ]
+    })
+  })
+
+  test('should throw error when lack end tag', () => {
+    expect(() => {
+      baseParse('<div><span></div>')
+    }).toThrowError('缺少结束标签span')
   })
 })
